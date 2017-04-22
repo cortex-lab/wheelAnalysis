ddf2ab0c434d915cd2d04172e185b861a1da2cda
nsteinme <nick.steinmetz@gmail.com>
2017-04-22 15:50:14 +0100

    add newer function to find movement onsets/offsets

---
+++ +wheel/findWheelMoves2.m
@@ -0,0 +1,65 @@
+
+
+
+function [moveTimes, moveAmp, movePeakVel] = findWheelMoves2(times, vel, pos, thresh, minBetweenMoves)
+% A different algorithm than findWheelMoves. 
+% Here, we look for a threshold crossing, then find the subsequent time
+% that the velocity is less than the threshold for some amount of time
+% (minBetweenMoves). That's the end of the movement, and the total
+% amplitude is computed as the difference in position between the end and
+% the start. 
+
+timesFs = mean(diff(times)); % assuming times/vel/pos are evenly sampled in time! Use interp1 to do this if it isn't true.
+
+velAboveThresh = vel>thresh | vel<-thresh;
+
+possibleMoveStarts = find( (~velAboveThresh(1:end-1) & velAboveThresh(2:end)) );
+
+
+betweenMovesSamps = ceil(minBetweenMoves/timesFs);
+isEndCondition = conv(double(velAboveThresh), ones(1, betweenMovesSamps), 'same')==0;
+                
+moveTimes = zeros(2, length(possibleMoveStarts));                
+moveAmp = zeros(1, length(possibleMoveStarts));    
+movePeakVel = zeros(1, length(possibleMoveStarts));  
+                
+pMoveStartInd = 1;
+moveInd = 1;
+lastEndTime = 0;
+try
+while pMoveStartInd<length(possibleMoveStarts)
+    if times(possibleMoveStarts(pMoveStartInd))<lastEndTime
+        % this possibleMoveStart happened during the last movement; just
+        % move on.
+        pMoveStartInd = pMoveStartInd+1;
+    else
+        
+        startSamp = possibleMoveStarts(pMoveStartInd);
+        endSamp = find(isEndCondition(possibleMoveStarts(pMoveStartInd)+1:end),1) + startSamp;
+        
+        if isempty(endSamp) % this can happen when the movement is going on at the time the recording ends
+            endSamp = length(isEndCondition);
+        end
+        
+        moveTimes(1, moveInd) = times(startSamp);
+        moveTimes(2, moveInd) = times(endSamp);
+        
+        moveAmp(moveInd) = pos(endSamp)-pos(startSamp);
+        thisVel = vel(startSamp:endSamp);
+        movePeakVel(moveInd) = thisVel(find(abs(thisVel)==max(abs(thisVel)),1));
+        
+        lastEndTime = moveTimes(2, moveInd);
+        moveInd = moveInd+1;
+        pMoveStartInd = pMoveStartInd+1;
+        
+    end
+    
+end
+catch me
+    keyboard
+end
+    
+% pick out just the entries of moveTimes we had actually filled
+moveTimes = moveTimes(:,1:moveInd-1);
+moveAmp = moveAmp(:,1:moveInd-1);
+movePeakVel = movePeakVel(:,1:moveInd-1);
