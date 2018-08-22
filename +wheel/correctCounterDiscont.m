169797868da3fe93e1581cb4d581cdc4f4d9cd34
nsteinme <nick.steinmetz@gmail.com>
2018-08-22 12:34:22 +0100

    fix orientation issues - and offsetgit status - in wheel correction

--- +wheel/correctCounterDiscont.m
+++ +wheel/correctCounterDiscont.m
@@ -4,8 +4,9 @@ function posOut = correctCounterDiscont(pos)
 % rotary encoder sometimes gets weird values. This fixes them. Algorithm
 % from CB. 
 
-posDiff = diff(pos)'; % compute velocity
+posDiff = diff(pos(:)); % compute velocity
 % correct for counter flow discontinuities
 posDiff(posDiff > 2^31) = posDiff(posDiff > 2^31) - 2^32;
 posDiff(posDiff < -2^31) = posDiff(posDiff < -2^31) + 2^32;
-posOut = cumsum([0 posDiff]); % back to position
+posOut = cumsum([0; posDiff]); % back to position
+posOut = reshape(posOut, size(pos))+pos(1);
